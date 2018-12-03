d5d78374dc3be5a665a044dea68a2c36bb2c2591
TantamiUK <admin@tantami.co.uk>
2018-12-03 21:21:17 +0000

    Page changes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-var counter = require('./count');
+var stuff = require('./stuff');
 
-console.log(counter(['shaun', 'bob', 'ryan']));
+console.log(stuff.counter(['shaun', 'bob', 'ryan']));
+console.log(stuff.adder(10, 32));
+console.log(stuff.pi);
+console.log(stuff.adder(stuff.pi, 5));
