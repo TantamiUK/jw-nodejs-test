d3278eff27fb1ccf79fe082124305caeba0cfa6c
TantamiUK <admin@tantami.co.uk>
2018-12-04 14:07:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
 var events = require('events');
-var myEmitter = new events.EventEmitter();
-
-myEmitter.on('someEvent', function(msg){
-    console.log(msg);
-});
-
-myEmitter.emit('someEvent', 'the event was emmited');
