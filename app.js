d4cc378c7e7f9f602898e4708bea9d766239c051
TantamiUK <admin@tantami.co.uk>
2018-12-04 15:16:03 +0000

    Event listener

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 var events = require('events');
+var utils = require('util');
+
+var Person = function(name){
+    this.name = name;
+};
+
+utils.inherits(Person, events.EventEmitter);
+
+var james = new Person('James');
+var mary = new Person('Mary');
+var ryan = new Person('ryan');
+
+var people = [james, mary, ryan];
+
+people.forEach(function(person){
+    person.on('speak', function(msg){
+        console.log(person.name + ' said: ' + msg)
+    });
+});
+
+james.emit('speak', 'hey dudes');
+james.emit('speak', 'I want a curry');
