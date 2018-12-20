daa8874f84945d70b0f85b50916eed3e5918afec
James Walsh <admin@tantami.co.uk>
2018-12-20 15:39:14 +0000

    Tutorial Complete

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    if(req.url === '/home' || req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else if(req.url === '/contact-us') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/contact.html').pipe(res);
-    }
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
+app.get('/', function(req, res){
+    res.render('index')
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('Server is listening on port 3000');
+
+app.get('/contact', function(req, res){
+    res.render('contact', {qs: req.query})
+});
+
+app.post('/contact', urlencodedParser, function(req, res){
+    console.log(req.body);
+    res.render('contact-success', {data: req.body})
+});
+
+app.get('/profile/:name', function(req, res){
+    var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'walking']}
+    res.render('profile', {person: req.params.name, data: data});
+});
+
+app.listen(3000);
